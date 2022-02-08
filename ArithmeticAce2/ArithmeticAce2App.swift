c8255cd0ef57d20c3467513fd4831fdb87bebbd5
winstonwang <wwang@lcs.on.ca>
2022-02-08 11:11:21 -0500

    Initial Commit

---
+++ ArithmeticAce2/ArithmeticAce2App.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticAce2App.swift
+//  ArithmeticAce2
+//
+//  Created by Winston Wang on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAce2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
