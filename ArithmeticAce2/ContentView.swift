1c59b9212f420190773cef98c53f1d9158911232
winstonwang <wwang@lcs.on.ca>
2022-02-08 11:58:48 -0500

    show x image when the answer it's wrong andcheck image when it's right check answer button disappear after check, new question button appear after answer checked

--- ArithmeticAce2/ContentView.swift
+++ ArithmeticAce2/ContentView.swift
@@ -8,9 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var multiplicand = Int.random(in: 1...12)
+    @State var multiplier = Int.random(in: 1...12)
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctProduct: Int {
+        return multiplicand * multiplier
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+                
+        VStack(spacing: 0) {
+            HStack {
+                Text("✕")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(multiplicand)")
+                    Text("\(multiplier)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack{
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                        //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.circle")
+                        .foregroundColor(.red)
+                        //        CONDITION1       condition2
+                        //        true              false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    }
+                
+               
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            ZStack{
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .opacity(answerChecked ? 0.0 : 1.0)
+                    .padding()
+                    .buttonStyle(.bordered)
+                
+                Button(action: {
+                    multiplicand = Int.random(in: 1...12)
+                    multiplier = Int.random(in: 1...12)
+                    answerChecked = false
+                    answerCorrect = false
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .opacity(answerChecked ? 1.0 : 0.0)
+            }
+
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+
+        
     }
 }
 
