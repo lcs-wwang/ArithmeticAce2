e8926968d9f63543aa912fd46e18c9d1d1fb50f6
winstonwang <wwang@lcs.on.ca>
2022-02-09 11:19:42 -0500

    Starting point

---
+++ ArithmeticAce2/DivisionView.swift
@@ -0,0 +1,134 @@
+//
+//  DivisionView.swift
+//  ArithmeticAce2
+//
+//  Created by Winston Wang on 2022-02-09.
+//
+
+import SwiftUI
+
+struct DivisionView: View {
+    
+    // MARK: Stored properties
+    @State var dividend = Int.random(in: 1...12)
+    @State var divisor = Int.random(in: 1...12)
+
+    // This string contains whatever the user types in
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the dividend, based on the randomly generated divisor and quotient?
+    var correctQuotient: Int {
+        return dividend / divisor
+    }
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("÷")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(dividend)")
+                    Text("\(divisor)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    //        CONDITION      true  false
+                        .opacity(answerCorrect == true ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                    //        CONDITION1         AND     CONDITION2         true  false
+                    //       answerChecked = true     answerCorrect = false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    
+                    
+                }
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let quotientGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+                    
+                    // Check the answer!
+                    if quotientGiven == correctQuotient {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has not been checked
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
+                
+                Button(action: {
+                    // Generate a new question
+                    dividend = Int.random(in: 1...12)
+                    divisor = Int.random(in: 1...12)
+
+                    // Reset properties that track what's happening with the current question
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the input field
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has been checked
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+                
+            }
+                        
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+        
+        
+    }
+}
+
+struct DivisionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DivisionView()
+    }
+}
---
+++ ArithmeticAce2/SubtractionView.swift
@@ -0,0 +1,135 @@
+//
+//  SubtractionView.swift
+//  ArithmeticAce2
+//
+//  Created by Winston Wang on 2022-02-09.
+//
+
+import SwiftUI
+
+struct SubtractionView: View {
+    
+    // MARK: Stored properties
+    @State var minuend = Int.random(in: 1...12)
+    @State var subtrahend = Int.random(in: 1...12)
+    
+    // This string contains whatever the user types in
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct difference?
+    var correctDifference: Int {
+        return minuend - subtrahend
+    }
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("-")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(minuend)")
+                    Text("\(subtrahend)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    //        CONDITION      true  false
+                        .opacity(answerCorrect == true ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                    //        CONDITION1         AND     CONDITION2         true  false
+                    //       answerChecked = true     answerCorrect = false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                }
+                
+                Spacer()
+                
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let differenceGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+                    
+                    // Check the answer!
+                    if differenceGiven == correctDifference {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has not been checked
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
+                
+                Button(action: {
+                    // Generate a new question
+                    minuend = Int.random(in: 1...12)
+                    subtrahend = Int.random(in: 1...12)
+                    
+                    // Reset properties that track what's happening with the current question
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the input field
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has been checked
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+                
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+        
+        
+    }
+}
+
+struct SubtractionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubtractionView()
+    }
+}
+
